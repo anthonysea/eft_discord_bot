ef91bb782434536e2658e6de48609efa6cd06f25
anthonysea <anthonyqchao@gmail.com>
2020-02-26 22:03:26 -0800

    implementing simple event handling for when a member joins and for responding to messages

--- bot.py
+++ bot.py
@@ -17,7 +17,23 @@ async def on_ready():
 
     print(
         f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id}'
+        f'{guild.name}(id: {guild.id})'
     )
 
-client.run(token)
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!deez':
+        response = 'deez nutz'
+        await message.channel.send(response)
+
+client.run(TOKEN)
