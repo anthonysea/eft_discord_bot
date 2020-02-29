d63447d45278b8cb8f226a4b5807d88e6126b2be
anthonysea <anthonyqchao@gmail.com>
2020-02-28 19:35:50 -0800

    Beginning to write functionality of eft bot including ammo charts, and fetching map images

--- bot.py
+++ bot.py
@@ -8,31 +8,22 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-# client = discord.Client()
-
-# @client.event
-# async def on_ready():
-#     for guild in client.guilds:
-#         if guild.name == GUILD:
-#             break
-
-#     print(
-#         f'{client.user} is connected to the following guild:\n'
-#         f'{guild.name}(id: {guild.id})'
-#     )
-
-# @client.event
-# async def on_member_join(member):
-#     await member.create_dm()
-#     await member.dm_channel.send(
-#         f'Hi {member.name}, welcome to my Discord server!'
-#     )
-
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='deez')
-async def deez(ctx):
-    response = 'deez nutz!'
-    await ctx.send(response)
+
+@bot.group()
+def eft(ctx):
+    if ctx.invoked_subcommands is None:
+        await ctx.send('No subcommand passed.')
+
+
+@eft.command(name='map')
+def _map(ctx, map_name: str) -> None:
+    '''Fetches the map image for the given map'''
+
+
+@eft.command(name='ammo')
+def _ammo(ctx, ammo_type: str) -> None:
+    '''Responds with the ammo chart for the given ammo type'''
 
 bot.run(TOKEN)
---
+++ utils.py
@@ -0,0 +1,7 @@
+import gspread, oauth2client
+
+
+def load_ammo_chart() -> dict:
+    
+
+def load_map_images(map_name: str) -> str:
